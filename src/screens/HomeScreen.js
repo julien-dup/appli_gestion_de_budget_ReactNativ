34ea160b178873c2ddc454118be096b03be540d9
Alexy.L <alexy.lepretre76@laposte.net>
2022-06-02 16:23:04 +0200

    ajout context

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useContext } from 'react'
+import GlobalContext from '../tools/GlobalContext'
+
 
 const HomeScreen = () => {
+
+
+  // UTILISATION DU CONTEXT (EXEMPLE EN LECTURE PUIS ECRITURE)
+  //const contextValue = useContext(GlobalContext)
+  //console.log(contextValue.user)
+  //contextValue.user = "Changement de nom ..."
+  //console.log(contextValue.user)
+
+
   return (
     <View>
       <Text>HomeScreen</Text>
