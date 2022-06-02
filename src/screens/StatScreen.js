c88f3fa59b5cd0fe6d28285fbfe710084f88054a
Alexy.L <alexy.lepretre76@laposte.net>
2022-06-02 16:23:47 +0200

    ajout context 2

--- src/screens/StatScreen.js
+++ src/screens/StatScreen.js
@@ -1,13 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useContext } from 'react'
-import GlobalContext from '../tools/GlobalContext'
+//import GlobalContext from '../tools/GlobalContext'
 
   
 const StatScreen = () => {
   
   // UTILISATION DU CONTEXT (EXEMPLE EN LECTURE)
-  const contextValue = useContext(GlobalContext)
-  console.log(contextValue.user)
+  //const contextValue = useContext(GlobalContext)
+  //console.log(contextValue.user)
 
   return (
     <View>
